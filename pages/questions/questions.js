1c6c124499839299934451a8d366c64b9ea1851e
zhouyaling <1006911224@qq.com>
2020-05-08 21:43:13 +0800

    加入答题卡也没

--- pages/questions/questions.js
+++ pages/questions/questions.js
@@ -1,11 +1,109 @@
 // pages/questions/questions.js
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    id:0
+    id:0,
+    showPops:false,
+    currentQuestion:0,
+    totalQuestion:2,
+    questionList:
+      {
+        total:10,
+        list:[
+          {
+            id:1,
+            title:'列表中唯一的字符串或者或者数字，且不会发生改变。',
+            answerList:[{
+              id:'A',
+              title:'大管家'
+            },{
+              id:'B',
+              title:'大蛇去'
+            },
+            {
+              id:'C',
+              title:'周烤猫'
+            },{
+              id:'D',
+              title:'安居客'
+            }],
+          },
+          {
+            id:2,
+            title:'当数据改变触发渲染层重新渲染的时候，会校正带有 key 的组件，框架会确保他们被重新排序，而不是重新创建，以确保使组件保持自身的状态，并且提高列表。',
+            answerList:[{
+              id:'A',
+              title:'大管家'
+            },{
+              id:'B',
+              title:'大蛇去'
+            },
+            {
+              id:'C',
+              title:'周烤猫'
+            },{
+              id:'D',
+              title:'安居客'
+            }],
+          }
+        ]
+      }
+  },
+
+  /**
+   * 答题菜单切换
+   */
+  menuOnChange(event){
+    switch(event.detail){
+      case 0:
+         wx.navigateBack({
+          delta:  1,
+        })
+        break;
+        case 1:
+          this.setData({
+            currentQuestion:this.data.currentQuestion<=1?0:this.data.currentQuestion - 1
+          })
+          break;
+        case 2:
+         this.setData({
+          currentQuestion:(this.data.currentQuestion + 1)>=this.data.totalQuestion?(this.data.totalQuestion-1):this.data.currentQuestion+1
+        })
+        // 提示已经答完
+        if(this.data.currentQuestion==this.data.totalQuestion-1){
+          Dialog.confirm({
+            title: '标题',
+            message: '弹窗内容',
+            asyncClose: true
+          })
+            .then(() => {
+              setTimeout(() => {
+                Dialog.close();
+              }, 1000);
+            })
+            .catch(() => {
+              Dialog.close();
+            });
+        }
+        break;
+      case 3:
+        this.setData({
+          showPops:true
+        })
+    }
+  },
+
+  /**
+   * 关闭答题卡
+   */
+  popsOnClose(){
+    this.setData({
+      showPops:false
+    })
   },
 
   /**
